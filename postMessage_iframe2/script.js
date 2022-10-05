3cde2fe7ab00fd032f94850e57b3acd85ef78b4e
GroovyHooked <thomas.cariot@dailymotion.com>
2022-10-06 00:14:35 +0200

    Refacto iframes

--- postMessage_iframe2/script.js
+++ postMessage_iframe2/script.js
@@ -2,31 +2,33 @@ const buttonIframe1 = document.getElementById('submitiframe1');
 const textNode = document.getElementById('message1');
 const display = document.querySelector('.display');
 const port = document.querySelector('.port');
-let myText;
-console.log(window.location.href);
+const messages = [];
+let message;
+
+port.innerText = extractOrigin(window.location.href);
+
 textNode.addEventListener('input', (event) => {
-  myText = event.target.value;
+  message = event.target.value;
 });
-const sendMesage = () => {
-  const textNode = document.getElementById('message1');
-  if (window && window.parent) {
-    window.parent.postMessage(
-      {
-        message: myText,
-      },
-      'http://127.0.0.1:5501'
-    );
-  }
-};
+
 buttonIframe1.addEventListener('click', sendMesage);
 
-const messages = [];
-
-addEventListener('message', function (e) {
+addEventListener('message', (e) => {
   if (e.data.message === undefined) return;
   messages.push(e.data.message);
   displaymessages(messages);
 });
+
+function sendMesage() {
+  if (window && window.parent) {
+    window.parent.postMessage(
+      {
+        message: message,
+      },
+      'http://127.0.0.1:5501'
+    );
+  }
+}
 
 function displaymessages(messages) {
   while (display.firstChild) display.removeChild(display.firstChild);
@@ -42,4 +44,3 @@ function extractOrigin(url) {
   const arr = url.match(regex);
   return arr[0];
 }
-port.innerText = extractOrigin(window.location.href);
