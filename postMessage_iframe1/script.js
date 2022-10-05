901127f35b950bbc0a16748478295e3841742742
GroovyHooked <thomas.cariot@dailymotion.com>
2022-10-06 01:15:32 +0200

    Trying to display messages with persistence

--- postMessage_iframe1/script.js
+++ postMessage_iframe1/script.js
@@ -1,8 +1,8 @@
-const buttonIframe1 = document.getElementById('submitiframe1');
+const buttonIframe = document.getElementById('submitiframe');
 const textNode = document.getElementById('message1');
 const display = document.querySelector('.display');
 const port = document.querySelector('.port');
-const messages = [];
+let messages;
 let message;
 
 port.innerText = extractOrigin(window.location.href);
@@ -11,12 +11,12 @@ textNode.addEventListener('input', (event) => {
   message = event.target.value;
 });
 
-buttonIframe1.addEventListener('click', sendMesage);
+buttonIframe.addEventListener('click', sendMesage);
 
 addEventListener('message', (e) => {
   if (e.data.message === undefined) return;
-  messages.push(e.data.message);
-  displaymessages(messages);
+  console.log('received in iframe 1', e.data.message);
+  displaymessages(e.data.message);
 });
 
 function sendMesage() {
@@ -25,17 +25,17 @@ function sendMesage() {
   }
 }
 
-function displaymessages(messages) {
-  while (display.firstChild) display.removeChild(display.firstChild);
-  messages.forEach((message) => {
-    const msg = document.createElement('p');
-    msg.innerText = message;
-    display.append(msg);
-  });
-}
-
 function extractOrigin(url) {
   const regex = /\d+\.\d+\.\d+\.\d+\:\d+/g;
   const arr = url.match(regex);
   return arr[0];
+}
+
+function displaymessages(messages) {
+  while (display.firstChild) display.removeChild(display.firstChild);
+  messages.forEach((message) => {
+    const msg = document.createElement('p');
+    msg.innerText = message[0] + ': ' + message[1];
+    display.append(msg);
+  });
 }
--- postMessage_parent/script.js
+++ postMessage_parent/script.js
@@ -1,17 +1,27 @@
-addEventListener('message', function (e) {
+const messages = [];
+const display = document.querySelectorAll('.display');
+console.log({ display });
+
+addEventListener('message', (e) => {
   handleMessage(e);
 });
 
 function handleMessage(event) {
   if (event.origin === 'http://127.0.0.1:5500') {
+    console.log({ message: event.data.message }, 'comes from 5500');
+    messages.push(['iframe1', event.data.message]);
+    //console.log(messages);
     window.frames[1].postMessage(
-      { message: event.data.message },
+      { message: messages },
       'http://127.0.0.1:5502'
     );
   }
   if (event.origin === 'http://127.0.0.1:5502') {
+    console.log({ message: event.data.message }, 'comes from 5502');
+    messages.push(['iframe2', event.data.message]);
+    //console.log(messages);
     window.frames[0].postMessage(
-      { message: event.data.message },
+      { message: messages },
       'http://127.0.0.1:5500'
     );
   }
