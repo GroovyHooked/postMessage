fe83d03bd864b091018ba9e3e6f6cb6e1d1df091
GroovyHooked <thomas.cariot@dailymotion.com>
2022-10-08 17:11:58 +0200

    Eliminate redundancy & data inserted in storage

--- postMessage_parent/script.js
+++ postMessage_parent/script.js
@@ -9,25 +9,27 @@ function handleMessage(event) {
   if (event.origin === 'http://127.0.0.1:5501') {
     console.log({ message: event.data.message }, 'comes from 5501');
     messages.push(['iframe1', event.data.message]);
-    window.frames[1].postMessage(
-      { message: messages },
-      'http://127.0.0.1:5502'
-    );
-    window.frames[0].postMessage(
-      { message: messages },
-      'http://127.0.0.1:5501'
-    );
+    sendpostMessages(storeAndExtractMessages, messages);
   }
   if (event.origin === 'http://127.0.0.1:5502') {
     console.log({ message: event.data.message }, 'comes from 5502');
     messages.push(['iframe2', event.data.message]);
-    window.frames[1].postMessage(
-      { message: messages },
-      'http://127.0.0.1:5502'
-    );
-    window.frames[0].postMessage(
-      { message: messages },
-      'http://127.0.0.1:5501'
+    sendpostMessages(storeAndExtractMessages, messages);
+  }
+}
+
+function storeAndExtractMessages(messages) {
+  localStorage.setItem('storage', JSON.stringify(messages));
+  const storage = JSON.parse(localStorage.getItem('storage'));
+  return storage;
+}
+
+function sendpostMessages(callback, messages) {
+  const ports = ['5501', '5502'];
+  for (let i = 0; i < 2; i++) {
+    window.frames[i].postMessage(
+      { message: callback(messages) },
+      `http://127.0.0.1:${ports[i]}`
     );
   }
 }
